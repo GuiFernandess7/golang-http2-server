349e2a0927196b0d5b19b732da6f1bb9cb4b07c2
Guilherme Sampaio <guilhermefs09@protonmail.com>
2025-11-19 16:13:58 -0300

    update: query params

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -7,22 +7,92 @@ import (
 	mw "apiproject/internal/api/middlewares"
 	"crypto/tls"
 	"path/filepath"
-	"time"
+	"sync"
+	"encoding/json"
+	"strings"
 )
 
-func execsHandler(w http.ResponseWriter, r *http.Request){
-	switch r.Method {
-		case http.MethodGet:
-			w.Write([]byte("Hello GET Method on Execs Route"))
-		case http.MethodPost:
-			fmt.Println("Query: ", r.URL.Query())
+type Teacher struct {
+	ID 			int
+	FirstName 	string
+	LastName 	string
+	Class 		string
+	Subject		string
+}
 
-			err := r.ParseForm()
-			if err != nil {
-				return
-			}
-			fmt.Println("Form data from POST: ", r.Form)
+var teachers = make(map[int]Teacher)
+var mutex = &sync.Mutex{}
+var nextID = 1
+
+func init() {
+	teachers[nextID] = Teacher {
+		ID: nextID,
+		FirstName: "John",
+		LastName: "Cena",
+		Class: "A",
+		Subject: "Fight",
 	}
+	nextID++
+	teachers[nextID] = Teacher {
+		ID: nextID,
+		FirstName: "Jake",
+		LastName: "Peralta",
+		Class: "B",
+		Subject: "Investigation",
+	}
+	nextID++
+	teachers[nextID] = Teacher {
+		ID: nextID,
+		FirstName: "Robert",
+		LastName: "Greene",
+		Class: "B",
+		Subject: "Biology",
+	}
+}
+
+func getTeachers(w http.ResponseWriter, r *http.Request){
+	fmt.Println(r.Method)
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusForbidden)
+		return
+	}
+
+	path := strings.TrimPrefix(r.URL.Path, "/teachers/")
+	idStr := strings.TrimSuffix(path, "/")
+	fmt.Println(idStr)
+
+	firstName := r.URL.Query().Get("first_name")
+	lastName := r.URL.Query().Get("last_name")
+
+	teacherList := make([]Teacher, 0, len(teachers))
+	for _, teacher := range teachers {
+		if (firstName == "" || teacher.FirstName == firstName) &&
+			(lastName == "" || teacher.LastName == lastName) {
+		teacherList = append(teacherList, teacher)
+		}
+	}
+
+	response := struct {
+		Status string `json:"status"`
+		Count int  	  `json:"count"`
+		Data []Teacher `json:"data"`
+	}{
+		Status: "success",
+		Count: len(teacherList),
+		Data: teacherList,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+type Middleware func(http.Handler) http.Handler
+
+func applyMiddlewares(handler http.Handler, middlewares ...Middleware) http.Handler {
+	for _, middleware := range middlewares {
+		handler = middleware(handler)
+	}
+	return handler
 }
 
 func main() {
@@ -33,27 +103,29 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/execs", execsHandler)
+	mux.HandleFunc("/teachers/", getTeachers)
 
 	tlsConfig := &tls.Config{
 		MinVersion: tls.VersionTLS12,
 	}
 
-	hppOptions := mw.HPPOptions{
-		CheckQuery: 			  true,
-		CheckBody:  		      true,
-		CheckBodyForContentType:  "application/x-www-form-urlencoded",
-		Whitelist:				  []string{"sortBy", "sortOrder", "name", "age", "class"},
-	}
-
-	middlewares := mw.ResponseTimeMiddleware(
-		mw.SecurityHeaders(
-			mw.Compression(mux),
-		),
-	)
-
-	rl := mw.NewRateLimiter(5, time.Minute)
-	secureMux := mw.HPP(hppOptions)(mw.Cors(rl.Middleware(middlewares)))
+	// hppOptions := mw.HPPOptions{
+	// 	CheckQuery: 			  true,
+	// 	CheckBody:  		      true,
+	// 	CheckBodyForContentType:  "application/x-www-form-urlencoded",
+	// 	Whitelist:				  []string{"sortBy", "sortOrder", "name", "age", "class"},
+	// }
+	//rl := mw.NewRateLimiter(5, time.Minute)
+	// secureMux := applyMiddlewares(
+	// 	mux,
+	// 	mw.HPP(hppOptions),
+	// 	mw.Compression,
+	// 	mw.SecurityHeaders,
+	// 	mw.ResponseTimeMiddleware,
+	// 	rl.Middleware,
+	// 	mw.Cors,
+	// )
+	secureMux := mw.SecurityHeaders(mux)
 
 	server := &http.Server{
 		Addr: port,
